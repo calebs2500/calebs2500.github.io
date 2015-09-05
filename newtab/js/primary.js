f1600f75f4446f0d8c3b34b5460264a0e47dc5af
calebs2500 <calebs2500@gmail.com>
2015-09-05 21:55:44 +1000

    newtab dev 2

--- newtab/js/primary.js
+++ newtab/js/primary.js
@@ -32,10 +32,91 @@ setInterval(GetClock,1000);
 //Clock Position Options
 function showPositionOption() {
 	document.getElementById("positionOptionWrapper").className = "shown";
+	document.getElementById("positionOption1").style.height = "30px";
+	document.getElementById("positionOption1").style.width = "30px";
+	document.getElementById("positionOption2").style.height = "30px";
+	document.getElementById("positionOption2").style.width = "30px";
+	document.getElementById("positionOption3").style.height = "30px";
+	document.getElementById("positionOption3").style.width = "30px";
+	document.getElementById("positionOption4").style.height = "30px";
+	document.getElementById("positionOption4").style.width = "30px";
+	document.getElementById("positionOption5").style.height = "30px";
+	document.getElementById("positionOption5").style.width = "30px";
+	document.getElementById("positionOption6").style.height = "30px";
+	document.getElementById("positionOption6").style.width = "30px";
+	document.getElementById("positionOption7").style.height = "30px";
+	document.getElementById("positionOption7").style.width = "30px";
+	document.getElementById("positionOption8").style.height = "30px";
+	document.getElementById("positionOption8").style.width = "30px";
+	document.getElementById("positionOption9").style.height = "30px";
+	document.getElementById("positionOption9").style.width = "30px";
 	document.getElementById("move").onclick = hidePositionOption;
 }
 
 function hidePositionOption() {
 	document.getElementById("positionOptionWrapper").className = "hidden";
+document.getElementById("positionOption1").style.height = "0px";
+	document.getElementById("positionOption1").style.width = "0px";
+	document.getElementById("positionOption2").style.height = "0px";
+	document.getElementById("positionOption2").style.width = "0px";
+	document.getElementById("positionOption3").style.height = "0px";
+	document.getElementById("positionOption3").style.width = "0px";
+	document.getElementById("positionOption4").style.height = "0px";
+	document.getElementById("positionOption4").style.width = "0px";
+	document.getElementById("positionOption5").style.height = "0px";
+	document.getElementById("positionOption5").style.width = "0px";
+	document.getElementById("positionOption6").style.height = "0px";
+	document.getElementById("positionOption6").style.width = "0px";
+	document.getElementById("positionOption7").style.height = "0px";
+	document.getElementById("positionOption7").style.width = "0px";
+	document.getElementById("positionOption8").style.height = "0px";
+	document.getElementById("positionOption8").style.width = "0px";
+	document.getElementById("positionOption9").style.height = "0px";
+	document.getElementById("positionOption9").style.width = "0px";	
 	document.getElementById("move").onclick = showPositionOption;
+}
+
+function clockTopLeft() {
+	document.getElementById("time").className = "topleft";
+	setTimeout(hidePositionOption);
+}
+
+function clockTop() {
+	document.getElementById("time").className = "top";
+	setTimeout(hidePositionOption);
+}
+
+function clockTopRight() {
+	document.getElementById("time").className = "topright";
+	setTimeout(hidePositionOption);
+}
+
+function clockLeft() {
+	document.getElementById("time").className = "left";
+	setTimeout(hidePositionOption);
+}
+
+function clockMiddle() {
+	document.getElementById("time").className = "middle";
+	setTimeout(hidePositionOption);
+}
+
+function clockRight() {
+	document.getElementById("time").className = "right";
+	setTimeout(hidePositionOption);
+}
+
+function clockBottomLeft() {
+	document.getElementById("time").className = "bottomleft";
+	setTimeout(hidePositionOption);
+}
+
+function clockBottom() {
+	document.getElementById("time").className = "bottom";
+	setTimeout(hidePositionOption);
+}
+
+function clockBottomRight() {
+	document.getElementById("time").className = "bottomright";
+	setTimeout(hidePositionOption);
 }
